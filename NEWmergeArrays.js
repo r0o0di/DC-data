5bc3d5e74ac234ca47c11760e87934165734f2ea
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-05-10 18:40:08 +0200

    Add files via upload

--- NEWmergeArrays.js
+++ NEWmergeArrays.js
@@ -139,6 +139,7 @@ const mergeArrays = (episodes, episodesData) => {
 
         const formattedBGM = item.BGM.map((bg, index) => {
             const comma = index < item.BGM.length - 1 ? ',' : '';
+            bg[3] = bg[3].replace(/ver\./gi, "Version");
             return `["${bg[0]}", "${bg[1]}", "${bg[2]}", "${bg[3]}", "${bg[4]}"]${comma}<br>`;
         }).join('');
 
